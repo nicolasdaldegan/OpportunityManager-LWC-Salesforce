880a77bb77ba64dd805d3c087121fcb69d934a88
Nicolas Daldegan <nicolasdaldega@gmail.com>
2025-11-03 14:08:51 -0300

    chore: adicionando jst config e lockfile

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,16 @@ const { jestConfig } = require('@salesforce/sfdx-lwc-jest/config');
 
 module.exports = {
     ...jestConfig,
-    modulePathIgnorePatterns: ['<rootDir>/.localdevserver']
+
+    modulePathIgnorePatterns: ['<rootDir>/.localdevserver'],
+
+    collectCoverage: true,
+    coverageDirectory: './coverage',
+
+    collectCoverageFrom: [
+        'force-app/main/default/lwc/**/*.js',
+        '!force-app/main/default/lwc/**/mock*.js',
+        '!force-app/main/default/lwc/**/generated/**',
+        '!force-app/main/default/lwc/**/*.test.js'
+    ]
 };
